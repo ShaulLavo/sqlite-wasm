e941cf0b0945519acdc433cd0e9a013cafb1c0eb
Shauls <shaul919191@gmail.com>
2025-12-13 09:22:09 +0200

    feat: Add type declaration for `sqlite3.wasm?url` and include `wasm.d.ts` in demo's `tsconfig.json`.

--- wasm.d.ts
+++ wasm.d.ts
@@ -2,3 +2,8 @@ declare module '*/sqlite3.wasm?url' {
   const url: string;
   export default url;
 }
+
+declare module 'sqlite-wasm/sqlite3.wasm?url' {
+  const url: string;
+  export default url;
+}
